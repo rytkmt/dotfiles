7569342d9ecfe0190249f9a004a597a3099108c5
rytkmt <r12tkmt@gmail.com>
2021-12-16 15:11:55 +0900

    a

--- lua/plugins/surround_nvim.lua
+++ lua/plugins/surround_nvim.lua
@@ -1,4 +1,34 @@
 require("surround").setup {
-  mappings_style = "surround",
-  -- prefix = "<S-s>"
+  load_keymaps = false,
+
+  quotes = {"'", '"', '`'},
+  brackets = {"(", '{', '['},
+  pairs = {
+    nestable = {
+      {"(", ")"},
+      {"[", "]"},
+      {"{", "}"},
+    },
+    linear = {
+      {"'", "'"},
+      {"`", "`"},
+      {'"', '"'}
+    }
+  },
 }
+
+local function map(mode, key, cmd)
+  vim.api.nvim_set_keymap(mode, key, cmd, { noremap = true })
+end
+
+-- Special Maps
+map("n", "ys", "<cmd>set operatorfunc=SurroundAddOperatorMode<cr>g@")
+-- Cycle surrounding quotes
+map("n", "cq", "<cmd>lua require'surround'.toggle_quotes()<cr>")
+-- Normal Mode Maps
+-- Surround Add
+map("v", "S", "<esc>gv<cmd>lua require'surround'.surround_add()<cr>")
+-- Surround Delete
+map("n", "ds", "<cmd>lua require'surround'.surround_delete()<cr>")
+-- Surround Replace
+map("n", "cs", "<cmd>lua require'surround'.surround_replace()<cr>")
